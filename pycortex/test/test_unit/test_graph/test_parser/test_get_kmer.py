4938dba59f72876a57cf7f2bfcafc2d7b361367f
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-15 15:09:15 +0100

    Increase coverage

--- pycortex/test/test_unit/test_graph/test_parser/test_get_kmer.py
+++ pycortex/test/test_unit/test_graph/test_parser/test_get_kmer.py
@@ -6,12 +6,12 @@ import pycortex.graph.parser as parser
 from pycortex.test.builder.graph.body import kmers, KmerRecord, as_edge_set
 
 
-class TestGetKmer(object):
+class TestDunderGetitemDunder(object):
     @given(s.data(),
            s.integers(min_value=1, max_value=3),
            s.integers(min_value=1, max_value=3),
            s.integers(min_value=0, max_value=5))
-    def test_get_records(self, data, kmer_size, num_colors, n_kmers):
+    def test_record_retrieval(self, data, kmer_size, num_colors, n_kmers):
         # given
         graph_builder = (builder.Graph()
                          .with_kmer_size(kmer_size)
@@ -65,3 +65,58 @@ class TestGetKmerForString(object):
         # when
         assert expected_kmer.kmer == cg.get_kmer_for_string('AAA').kmer
         assert expected_kmer.kmer == cg.get_kmer_for_string('TTT').kmer
+
+
+class TestDunderIterDunder(object):
+    def test_gets_aaa(self):
+        # given
+        graph_builder = builder.Graph()
+        graph_builder.with_kmer_size(3)
+        graph_builder.with_num_colors(1)
+
+        expected_kmer = KmerRecord('AAA', (1,), (as_edge_set('........'),))
+        graph_builder.with_kmer_record(expected_kmer)
+
+        cg = parser.RandomAccess(graph_builder.build())
+
+        # when
+        for kmer in cg:
+            assert expected_kmer.kmer == kmer.kmer
+            assert expected_kmer.coverage == kmer.coverage
+            assert expected_kmer.edges == kmer.edges
+
+    def test_works_with_no_kmers(self):
+        # given
+        graph_builder = (builder.Graph()
+                         .with_kmer_size(3)
+                         .with_num_colors(1))
+
+        cg = parser.RandomAccess(graph_builder.build())
+
+        # when/then
+        assert list(cg) == []
+
+    @given(s.data(),
+           s.integers(min_value=1, max_value=10),
+           s.integers(min_value=1, max_value=10),
+           s.integers(min_value=0, max_value=4))
+    def test_parses_records(self, data, kmer_size, num_colors, n_kmers):
+        # given
+        graph_builder = (builder.Graph()
+                         .with_kmer_size(kmer_size)
+                         .with_num_colors(num_colors))
+
+        expected_kmers = []
+        for _ in range(n_kmers):
+            kmer = data.draw(kmers(kmer_size, num_colors))
+            graph_builder.with_kmer_record(kmer)
+            expected_kmers.append(kmer)
+        expected_kmers = sorted(expected_kmers)
+        ra_parser = parser.RandomAccess(graph_builder.build())
+
+        # when
+        for kmer, expected_kmer in zip(ra_parser, expected_kmers):
+            # then
+            assert expected_kmer.kmer == kmer.kmer
+            assert expected_kmer.coverage == kmer.coverage
+            assert expected_kmer.edges == kmer.edges
