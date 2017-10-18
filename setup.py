fba496a8d22e3a75ec481f12b286ae2bb6ae1bec
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-18 14:17:03 +0100

    Add package requirements from Pipfile.lock to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+import os
 from setuptools import setup, find_packages
+import json
+
+
+def get_requirements_from_pipfile_lock(pipfile_lock=None):
+    if pipfile_lock is None:
+        pipfile_lock = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Pipfile.lock')
+    lock_data = json.load(open(pipfile_lock))
+    return [package_name for package_name in lock_data.get('default', {}).keys()]
+
+
+packages = find_packages('.', exclude=['*.test', '*.test.*'])
+pipfile_lock_requirements = get_requirements_from_pipfile_lock()
 
 setup(
     name='pycortex',
-    version='0.0.1',
-    packages=find_packages('pycortex'),
+    version='0.0.2',
+    packages=packages,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=pipfile_lock_requirements,
     python_requires=">=3.5",
 )
