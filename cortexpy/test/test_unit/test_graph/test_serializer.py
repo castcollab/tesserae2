c9ad14ffa529f1831589998b93c01c1708ec5478
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-01-25 10:28:22 +0100

    WIP

--- cortexpy/test/test_unit/test_graph/test_serializer.py
+++ cortexpy/test/test_unit/test_graph/test_serializer.py
@@ -1,4 +1,7 @@
+import io
 import json
+
+import networkx as nx
 
 import cortexpy.graph as graph
 import cortexpy.graph.serializer as serializer
@@ -246,3 +249,29 @@ class TestToJson(object):
         kmer_data = json.loads(kmer_json)  # does not raise
         assert kmer_data['graph']['colors'] == list(range(3))
         assert kmer_data['graph']['sample_names'] == list(color_names) + ['retrieved_contig']
+
+
+class TestPickle(object):
+    def test_two_linked_kmers_pickle_ok(self):
+        # given
+        color_names = 'samp1', 'samp2'
+        graph_builder = builder.Graph()\
+            .with_kmer_size(3)\
+            .with_num_colors(2)\
+            .with_color_names(*color_names)\
+            .with_kmer('AAA', [1, 1], ['.....C..', '.......T'])\
+            .with_kmer('AAC', [1, 0], ['a.......', '........'])
+        retriever = graph.ContigRetriever(graph_builder.build())
+        kmer_graph = retriever.get_kmer_graph('GTTT')
+
+        # when
+        buffer = io.BytesIO()
+        nx.write_gpickle(kmer_graph, buffer)
+        buffer.seek(0)
+        unpickled_kmer_graph = nx.read_gpickle(buffer)
+
+        # then
+        assert len(unpickled_kmer_graph) == len(kmer_graph)
+        unpickle_node_data = unpickled_kmer_graph.nodes(data=True)
+        for node, data in kmer_graph.nodes(data=True):
+            assert unpickle_node_data[node] == data
