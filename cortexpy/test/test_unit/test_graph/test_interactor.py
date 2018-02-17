c964d1a69a9dc8a2b926962049b52b7a0117c1be
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-02-17 16:51:36 +0000

    Acceptance tests for assembly pass

---
+++ cortexpy/test/test_unit/test_graph/test_interactor.py
@@ -0,0 +1,43 @@
+import networkx as nx
+
+from cortexpy.graph import interactor
+
+
+def test_only_follows_one_color_with_color_specified():
+    # given
+    graph = nx.MultiDiGraph()
+    graph.add_edge('AAA', 'AAT', key=0)
+    graph.add_edge('AAT', 'ATA', key=1)
+
+    # when
+    paths = list(interactor.Contigs(graph, color=0).all_simple_paths())
+
+    # then
+    assert ['AAAT'] == [str(p.seq) for p in paths]
+
+
+def test_follows_two_colors_with_no_color_specified():
+    # given
+    graph = nx.MultiDiGraph()
+    graph.add_edge('AAA', 'AAT', key=0)
+    graph.add_edge('AAT', 'ATA', key=1)
+
+    # when
+    paths = list(interactor.Contigs(graph).all_simple_paths())
+
+    # then
+    assert ['AAATA'] == [str(p.seq) for p in paths]
+
+
+def test_follows_three_colors_with_no_color_specified():
+    # given
+    graph = nx.MultiDiGraph()
+    graph.add_edge('AAA', 'AAT', key=0)
+    graph.add_edge('AAT', 'ATA', key=1)
+    graph.add_edge('AAT', 'ATC', key=2)
+
+    # when
+    paths = list(interactor.Contigs(graph).all_simple_paths())
+
+    # then
+    assert ['AAATA', 'AAATC'] == [str(p.seq) for p in paths]
--- cortexpy/test/test_unit/test_graph/test_serializer.py
+++ cortexpy/test/test_unit/test_graph/test_serializer.py
@@ -1,74 +1,9 @@
 import json
 
-import attr
-
 import cortexpy.graph as graph
 import cortexpy.graph.serializer as serializer
 import cortexpy.test.builder as builder
-from cortexpy.graph import traversal, parser
-from cortexpy.test.expectation.kmer import CollapsedKmerUnitgGraphExpectation
-
-
-@attr.s(slots=True)
-class SerializerTestDriver(object):
-    graph_builder = attr.ib(attr.Factory(builder.Graph))
-    contig_to_retrieve = attr.ib(None)
-    retriever = attr.ib(None)
-    traverse = attr.ib(False)
-    retrieve = attr.ib(False)
-    traversal_start_kmer = attr.ib(None)
-    traversal_colors = attr.ib((0,))
-
-    def with_kmer_size(self, n):
-        self.graph_builder.with_kmer_size(n)
-        return self
-
-    def with_kmer(self, *args):
-        self.graph_builder.with_kmer(*args)
-        return self
-
-    def traverse_with_start_kmer_and_color(self, start_kmer, color):
-        self.traverse = True
-        self.traversal_start_kmer = start_kmer
-        self.traversal_colors = [color]
-        return self
-
-    def retrieve_contig(self, contig):
-        self.retrieve = True
-        self.contig_to_retrieve = contig
-        return self
-
-    def run(self):
-        if self.retrieve:
-            self.retriever = graph.ContigRetriever(self.graph_builder.build())
-            return self.retriever.get_kmer_graph(self.contig_to_retrieve)
-        elif self.traverse:
-            traverser = traversal.Engine(parser.RandomAccess(self.graph_builder.build()),
-                                         traversal_colors=self.traversal_colors)
-            return traverser.traverse_from(self.traversal_start_kmer).graph
-        else:
-            raise Exception("Need to load a command")
-
-
-@attr.s(slots=True)
-class CollapseKmerUnitigsTestDriver(object):
-    serializer_driver = attr.ib(attr.Factory(SerializerTestDriver))
-
-    def __getattr__(self, name):
-        serializer_method = getattr(self.serializer_driver, name)
-
-        def method(*args):
-            serializer_method(*args)
-            return self
-
-        return method
-
-    def run(self):
-        kmer_graph = self.serializer_driver.run()
-        collapser = (serializer
-                     .UnitigCollapser(kmer_graph)
-                     .collapse_kmer_unitigs())
-        return CollapsedKmerUnitgGraphExpectation(collapser.unitig_graph)
+from cortexpy.test.driver.graph.serializer import CollapseKmerUnitigsTestDriver
 
 
 class TestCollapseKmerUnitigsCreatesSingleUnitig(object):
