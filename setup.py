cd08e864730924fbd0aaeb237c49fe8b4fc5d03f
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-08-29 10:27:44 +0200

    Bump version: 0.38.1 → 0.39.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extensions = [Extension(
 if USE_CYTHON:
     extensions = cythonize(extensions)  # , annotate=True)
 
-version = '0.38.1'
+version = '0.39.0'
 setup(
     name='cortexpy',
     version=version,
